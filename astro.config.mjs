465cba24e3d52bfdf54a954d1e48a74177499694
johnrobmiller29 <johnrobmiller29@gmail.com>
2026-02-12 14:13:39 -0700

    style: add alt descriptions to activites.json, update index.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,7 +9,6 @@ import icon from "astro-icon";
 export default defineConfig({
   site: "https://johnrobmiller29.github.io",
   base: "/",
-  trailingSlash: "always",
   vite: {
     plugins: [tailwindcss()],
   },
